98c13f68c26e46c0c210e44dace81c05b2a7017a
Grisha <guruwolf8@gmail.com>
2023-05-25 15:42:12 +0300

    Домашка,задача 2.

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        task1();}
-        public static void task1(){
-            int a=123456;
-            byte b=1;
-            short c=150;
-            long d=145423566445L;
-            float e=3.23f;
-            double f=3.435234562345d;
-            System.out.println("Значение переменной (а), с типом int равно "+a);
-            System.out.println("Значение переменной (b), с типом byte равно "+b);
-            System.out.println("Значение переменной (c), с типом short равно "+c);
-            System.out.println("Значение переменной (d), с типом long равно "+d);
-            System.out.println("Значение переменной (e), с типом float равно "+e);
-            System.out.println("Значение переменной (а), с типом int равно "+f);
+        task1();
+    task2();}
+        public static void task1() {
+            int a = 123456;
+            byte b = 1;
+            short c = 150;
+            long d = 145423566445L;
+            float e = 3.23f;
+            double f = 3.435234562345d;
+            System.out.println("Значение переменной (а), с типом int равно " + a);
+            System.out.println("Значение переменной (b), с типом byte равно " + b);
+            System.out.println("Значение переменной (c), с типом short равно " + c);
+            System.out.println("Значение переменной (d), с типом long равно " + d);
+            System.out.println("Значение переменной (e), с типом float равно " + e);
+            System.out.println("Значение переменной (а), с типом int равно " + f);
 
-
+        }
+        public static void task2(){//В каждом методе могу называть переменные как в прошлых.
+        float a=27.12f;
+        long b=987678965549L;
+        double c=2.786d;
+        int d=569;
+        short e=-159;
+        int f=27897;
+        byte g=67;
+        //В условие не сказано про вывод,но желтая лампа мне не нравится.
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+            System.out.println(e);
+            System.out.println(f);
+            System.out.println(g);
     }
 }
