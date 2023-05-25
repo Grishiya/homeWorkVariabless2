f4a76e68a7de6c93bd80d37f2ad6536778fe2bbd
Grisha <guruwolf8@gmail.com>
2023-05-25 15:34:49 +0300

    Домашка,задача 1.

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();}
+        public static void task1(){
+            int a=123456;
+            byte b=1;
+            short c=150;
+            long d=145423566445L;
+            float e=3.23f;
+            double f=3.435234562345d;
+            System.out.println("Значение переменной (а), с типом int равно "+a);
+            System.out.println("Значение переменной (b), с типом byte равно "+b);
+            System.out.println("Значение переменной (c), с типом short равно "+c);
+            System.out.println("Значение переменной (d), с типом long равно "+d);
+            System.out.println("Значение переменной (e), с типом float равно "+e);
+            System.out.println("Значение переменной (а), с типом int равно "+f);
+
+
+    }
+}
